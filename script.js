716e6f526e75897eb2265e2282fe90dd7d163b6c
Tatjana Brandl <tatjanabrandl@gmail.com>
2020-09-28 22:07:47 +0200

    opdracht035_DOM_Manipulatie deel 3

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const buttons = document.querySelectorAll('.big-five-button')
 buttons.forEach(button => button.addEventListener('click', function () {
 
     const listElement = document.createElement('li')
-    listElement.classList.add("spotted-animals-list-item");
-    const list = document.getElementById("spotted-animals-list");
+    listElement.classList.add('spotted-animals-list-item');
+    const list = document.getElementById('spotted-animals-list');
     list.appendChild(listElement);
     listElement.innerText = button.innerText
 }))
@@ -15,4 +15,13 @@ removeFirstItem.addEventListener('click', function () {
     let spottedAnimalsListItem = document.querySelector('.spotted-animals-list-item')
     let getParent = spottedAnimalsListItem.parentNode
     getParent.removeChild(spottedAnimalsListItem)
+})
+
+const removeAllElements = document.getElementById('remove-all-button');
+
+removeAllElements.addEventListener('click', function () {
+    let spottedAnimalsListItem = document.querySelector('.spotted-animals-list-item')
+    let getParent = spottedAnimalsListItem.parentNode
+    getParent.innerHTML = "";
+
 })
