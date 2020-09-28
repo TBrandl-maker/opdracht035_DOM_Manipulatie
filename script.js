b4c8235ce0349c1deb560c7d4b93dd429e3fcace
Tatjana Brandl <tatjanabrandl@gmail.com>
2020-09-28 21:51:03 +0200

    opdracht035_DOM_Manipulatie deel 2

--- script.js
+++ script.js
@@ -2,9 +2,17 @@ const buttons = document.querySelectorAll('.big-five-button')
 
 buttons.forEach(button => button.addEventListener('click', function () {
 
-    const listElement = document.createElement("li")
+    const listElement = document.createElement('li')
     listElement.classList.add("spotted-animals-list-item");
     const list = document.getElementById("spotted-animals-list");
     list.appendChild(listElement);
     listElement.innerText = button.innerText
 }))
+
+const removeFirstItem = document.getElementById('remove-first-item-button')
+
+removeFirstItem.addEventListener('click', function () {
+    let spottedAnimalsListItem = document.querySelector('.spotted-animals-list-item')
+    let getParent = spottedAnimalsListItem.parentNode
+    getParent.removeChild(spottedAnimalsListItem)
+})
